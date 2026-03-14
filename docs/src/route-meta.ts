6df4163f66be36ef40302f4c81d9b58ad912a972
beatzball <github@beatzball.com>
2026-03-14 05:07:32 -0500

    docs: add umami

--- docs/src/route-meta.ts
+++ docs/src/route-meta.ts
@@ -12,9 +12,10 @@ export const starlightHead = [
   '<link rel="stylesheet" href="/shoelace/themes/light.css" />',
   '<link rel="stylesheet" href="/styles/starlight.css" />',
   '<link rel="stylesheet" href="/styles/highlight.css" />',
-  '<script>(function(){',
+  '<script defer src="https://umami.litro.dev/script.js" data-website-id="b76614a6-2fb4-4f8c-b279-0075a2d54067"></script>',
+  "<script>(function(){",
   'var s=localStorage.getItem("sl-theme");',
   'var t=s||(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light");',
   'document.documentElement.setAttribute("data-theme",t);',
-  '})();</script>',
-].join('');
+  "})();</script>",
+].join("");
